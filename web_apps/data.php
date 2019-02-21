ca53c208df9e5a428faf474667e637c3024da495
TheBryanJohnson <31318860+TheBryanJohnson@users.noreply.github.com>
2019-02-21 15:13:30 -0500

    Add files via upload

---
+++ web_apps/data.php
@@ -0,0 +1,7 @@
+
+<?php
+function get_overwatch_schedule(){
+//from data to stages to matches to competitors to name
+curl https://api.overwatchleague.com/schedule
+}
+?>
---
+++ web_apps/home.php
@@ -0,0 +1,13 @@
+<html>
+	<h1>
+		Overwatch League Schedule
+	</h1>
+<body>
+
+<form action="home.php" method="GET">
+<input id="search" type="text" placeholder="Type here">
+<input id="submit" type="submit" value="Search">
+
+</form>
+</body>
+</html>
